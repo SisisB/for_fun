0bb92b78d512f3ee7640b72408ad260933d14443
SisisB <165931589+SisisB@users.noreply.github.com>
2024-04-03 19:19:26 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    
+    var temperature = 10;
+
+    if (temperature <=5) {
+      System.out.println("Heat up room");
+    }
+    else if (temperature <= 15) {
+      System.out.println("Its warmer");
+    }
+    else if (temperature <= 30) {
+      System.out.println("Hot in there?");
+    }
+   
+    else{
+      System.out.println("Where are You? ");
+    }
+}
+}
